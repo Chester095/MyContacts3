b82a1bc78a14479229dd398589fd838051c5485a
Chester095 <50018523+Chester095@users.noreply.github.com>
2021-01-20 14:02:05 +0300

    Создали все аннотации

--- app/src/main/java/com/example/mycontacts3/ContactDAO.java
+++ app/src/main/java/com/example/mycontacts3/ContactDAO.java
@@ -1,21 +1,30 @@
 package com.example.mycontacts3;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
 public interface ContactDAO {
 
-    @Insert //lj,
+    @Insert //добавление контактов
     void insertContact(Contact contact);
 
     @Query("Select * FROM contacts_table") // для получения всех контактов
     List<Contact> getAllContacts();
 
-    @Query("SELECT * FROM contacts_table WHERE concatId==:contactId") //
-    Contact getContact(Long contactId)
+    @Query("SELECT * FROM contacts_table WHERE concatId==:contactId") //метод который возвращает данные
+    Contact getContact(Long contactId);
+
+    @Update
+    void updateContact(Contact contact);
+
+    @Delete
+    void deleteContact(Contact contact);
+
 
 }
